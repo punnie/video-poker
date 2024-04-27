1d4b094ed0e48fb71fee33e043b9c2329cd5a864
Pedro Coelho <punnie@ethereal.io>
2024-04-27 02:02:58 +0100

    Detect straights and jacks or higher

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+  "cmp"
+  "slices"
   "fmt"
   "math/rand"
   "time"
@@ -28,6 +30,7 @@ var (
 
 type card struct {
   rank string
+  rank_value int
   suite string
 }
 
@@ -39,8 +42,8 @@ func initializeDeck() []card {
   var deck []card
 
   for _, suite := range suites {
-    for _, rank := range ranks {
-      deck = append(deck, card{rank: rank, suite: suite})
+    for i, rank := range ranks {
+      deck = append(deck, card{rank: rank, rank_value: i, suite: suite})
     }
   }
 
@@ -113,12 +116,23 @@ type prize struct {
 }
 
 func (p prize) toString() string {
-  return [...]string{"NONE", "JACKS", "TWO PAIR", "THREE OF A KIND", "STRAIGHT", "FLUSH", "FULL HOUSE", "FOUR OF A KIND", "STRAIGHT FLUSH", "ROYAL FLUSH"}[p.hand]
+  return [...]string{"NONE", "JACKS OR HIGHER", "TWO PAIR", "THREE OF A KIND", "STRAIGHT", "FLUSH", "FULL HOUSE", "FOUR OF A KIND", "STRAIGHT FLUSH", "ROYAL FLUSH"}[p.hand]
 }
 
-func detectPrize(hand []card) prize {
+func detectPrize(h []card) prize {
   ranks := map[string]int{}
   suites := map[string]int{}
+
+  hand := make([]card, len(h))
+  copy(hand, h)
+
+  cardCmp := func(a, b card) int {
+    return cmp.Compare(a.rank_value, b.rank_value)
+  }
+
+  slices.SortFunc(hand, cardCmp)
+
+  // fmt.Println("Sorted hand: ", hand)
 
   for _, card := range hand {
     _, rank_is_present := ranks[card.rank]
@@ -136,38 +150,78 @@ func detectPrize(hand []card) prize {
     }
   }
 
-  fmt.Println("Ranks: ", ranks)
-  fmt.Println("Ranks length: ", len(ranks))
-  fmt.Println("Suites: ", suites)
-  fmt.Println("Suites length: ", len(suites))
+  // fmt.Println("Ranks: ", ranks)
+  // fmt.Println("Ranks length: ", len(ranks))
+  // fmt.Println("Suites: ", suites)
+  // fmt.Println("Suites length: ", len(suites))
+
+  is_straight := true
+
+  // Check for straight
+  for i := 0; i < len(hand) - 1; i++ {
+    if hand[i + 1].rank_value != hand[i].rank_value + 1 {
+      is_straight = false
+    }
+  }
 
   // We got ourselves some type of flush
   if len(suites) == 1 {
+    if is_straight {
+      // Royal straight flush
+      if hand[len(hand) - 1].rank_value == 12 {
+        return prize{hand: 9}
+      }
+
+      // Straight flush
+      return prize{hand: 8}
+    }
+
+    // Flush
     return prize{hand: 5}
   }
 
   // We either got four of a kind or a full house
   if len(ranks) == 2 {
     for _, v := range ranks {
+      // Four of a kind
       if v == 4 {
         return prize{hand: 7}
       }
     }
 
+    // Full house
     return prize{hand: 6}
+  }
+
+  // Straight
+  if is_straight {
+    return prize{hand: 4}
   }
 
   // We likely have a three of a kind or two pair
   if len(ranks) == 3 {
     for _, v := range ranks {
+      // Three of a kind
       if v == 3 {
         return prize{hand: 3}
       }
     }
 
+    // Two pair
     return prize{hand: 2}
   }
 
+  // One pair: check if jacks or higher
+  if len(ranks) == 4 {
+    for r, v := range ranks {
+      // Jacks or higher
+      if v == 2 && ( r == "J" || r == "Q" || r == "K" || r == "A") {
+        return prize{hand: 1}
+      }
+    }
+  }
+
+  // No prize
   return prize{hand: 0}
 }
 
